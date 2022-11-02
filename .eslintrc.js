9480b0433cf5a49b6e59d8b004212f09a46a6629
kuake <>
2022-11-02 12:31:19 +0800

    [update] add element-ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,25 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+  // 基于插件的编码规则
   extends: [
     'plugin:vue/essential',
     '@vue/standard',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    '@vue/typescript'
   ],
+  // 解析器
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser'
   },
+  // 自定义编码规则
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
+
 }
